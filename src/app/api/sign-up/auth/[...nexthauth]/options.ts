5e6615ac0fc8617cbcc74c9fa54eab036226c5f6
sambh256 <sambhavpowar@gmail.com>
2025-05-23 13:49:42 +0530

    next auth

---
+++ src/app/api/sign-up/auth/[...nexthauth]/options.ts
@@ -0,0 +1,62 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+
+export const authOptions:NextAuthOptions={
+    providers:[
+        CredentialsProvider({
+            id:"credentials",
+            name:"Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials:any):Promise<any>{
+                await dbConnect()
+                try{
+                    const user=await UserModel.findOne({
+                        $or:[
+                            {email:credentials.identifier},
+                            {username:credentials.identifier}
+                        ]
+                    })
+                    if(!user){
+                        throw new Error('No user found with this email')
+                    }
+                    if(!user.isVerified){
+                        throw new Error('User not verified please verify account before login')
+                    }
+                    const isPasswordCorrect=await bcrypt.compare(credentials.password,user.password)
+                    if(isPasswordCorrect){
+                        return user
+                    }
+                    else{
+                        throw new Error('wrong password')
+                    }
+                    
+                }catch(err:any){
+                    throw new Error(err)
+                }
+            }
+        })
+    ],
+    callbacks:{
+        async session({ session,  token }) {
+            return session
+        },
+        async jwt({ token, user}) {
+            
+
+            return token
+        }
+    },
+    pages:{
+        signIn: '/sign-in',
+    },
+    session:{
+        strategy:"jwt"
+    },
+    secret:process.env.NEXTAUTH_SECRET,
+}
